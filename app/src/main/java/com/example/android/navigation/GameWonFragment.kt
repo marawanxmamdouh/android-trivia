b5bd9155e91ab66a5f86d6ca28e38b4a36b95901
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-09 20:17:17 +0200

    Adding Safe Arguments between fragments

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -35,6 +36,10 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        // get the arguments from GameFragment
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "numCorrect = ${args.numCorrect}, totalAnswers = ${args.numQuestions}", Toast.LENGTH_SHORT).show()
         return binding.root
     }
 }
