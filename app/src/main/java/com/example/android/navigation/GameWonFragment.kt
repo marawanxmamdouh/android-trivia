b747223e60a8cb48723a1574a0a58afa13110b81
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-11 06:29:05 +0200

    Clean the project and reformat files

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -1,45 +1,34 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.navigation
 
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
-
 class GameWonFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_game_won, container, false)
+            inflater, R.layout.fragment_game_won, container, false
+        )
         binding.nextMatchButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
 
         // get the arguments from GameFragment
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context, "numCorrect = ${args.numCorrect}, totalAnswers = ${args.numQuestions}", Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+            context,
+            "numCorrect = ${args.numCorrect}, totalAnswers = ${args.numQuestions}",
+            Toast.LENGTH_SHORT
+        ).show()
 
         setHasOptionsMenu(true)
         return binding.root
@@ -50,7 +39,7 @@ class GameWonFragment : Fragment() {
         inflater.inflate(R.menu.winner_menu, menu)
 
         // check if the activity resolves
-        if (null == requireActivity().packageManager.resolveActivity(getShareIntent(),0)) {
+        if (null == requireActivity().packageManager.resolveActivity(getShareIntent(), 0)) {
             // hide the menu item if it doesn't resolve
             menu.findItem(R.id.share).isVisible = false
         }
@@ -65,7 +54,10 @@ class GameWonFragment : Fragment() {
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
         val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.setType("text/plain")
-                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .putExtra(
+                Intent.EXTRA_TEXT,
+                getString(R.string.share_success_text, args.numCorrect, args.numQuestions)
+            )
         return shareIntent
     }
 
